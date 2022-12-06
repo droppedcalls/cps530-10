bf9f2fdc46b0c70392b6c108a70457f39b1062c8
droppedcalls <bill.wang@outlook.com>
2022-12-05 20:08:47 -0500

    More links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 function App() {
   let tutorial = "https://reactjs.org/docs/create-a-new-react-app.html#create-react-app";
+  let github = "https://github.com/droppedcalls/cps530-10";
   let netlify = "https://www.netlify.com";
   let netlifyCustom = "https://docs.netlify.com/domains-https/netlify-dns/delegate-a-subdomain-to-netlify-dns/";
   return (
@@ -14,7 +15,8 @@ function App() {
       <div className="App-body">
         <p>
           This is a React web page built by following the <a href={tutorial} className='App-link'>official React app tutorial</a>. (I already had Node.js installed)<br/>
-          The app was then pushed onto a GitHub repository, which is then pulled from by <a href={netlify} className='App-link'>Netlify</a> and deployed to the current server.
+          The app was then pushed onto a <a href={github} className='App-link'>GitHub repository</a>, which is then pulled from 
+          by <a href={netlify} className='App-link'>Netlify</a> and deployed to the current server.
         </p>
         <h3>Problems and difficulties</h3>
         <p>
